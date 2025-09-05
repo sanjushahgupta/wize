b2663649e330f1e86095acf1355f6b0a9823b135
Sanju <sanjushahgupta@gmail.com>
2025-09-05 09:11:34 +0200

    add repository layer

--- internal/domain/transactions.go
+++ internal/domain/transactions.go
@@ -1,15 +1,18 @@
 package domain
 
+import "wize/internal/repository"
+
 type Transaction struct {
 	Date        string `json:"date"`
 	Amount      string `json:"amount"`
 	Description string `json:"description"`
+	From        string `json:"from"`
+	To          string `json:"to"`
+	Category    string `json:"category"`
 }
 
 func GetAllTransactions() []Transaction {
-	return []Transaction{
-		{Date: "Jan 1", Amount: "$200", Description: "Rent"},
-		{Date: "Jan 2", Amount: "$12", Description: "Coffee"},
-		{Date: "Jan 3", Amount: "$30", Description: "Lunch"},
-	}
+	transactions := repository.SelectAll()
+
+	return transactions
 }
---
+++ internal/repository/transactions.go
@@ -0,0 +1,22 @@
+package repository
+
+import (
+	"encoding/json"
+	"os"
+	"wize/internal/domain"
+)
+
+func SelectAll() ([]domain.Transaction, error) {
+	var transactions []domain.Transaction
+
+	data, err := os.ReadFile("db.json")
+	if err != nil {
+		return transactions, err
+	}
+
+	if err := json.Unmarshal(data, &transactions); err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
